f1854f346dc8e499cb6c0d772b38e842f46e4613
Carson Zhang <carsonzhang4@gmail.com>
2024-05-31 16:24:26 +0200

    added corruptmnist_v2, containing 20000 new images

--- corruptmnist_cookiecutter/data/make_dataset.py
+++ corruptmnist_cookiecutter/data/make_dataset.py
@@ -1,6 +1,11 @@
 import click
 import torch
 
+@click.group()
+def cli():
+    """Command line interface."""
+    pass
+
 def standardize(images: torch.Tensor) -> torch.Tensor:
     """Standardize images."""
     return (images - images.mean()) / images.std()
@@ -22,8 +27,8 @@ def make_data(raw_dir: str, processed_dir: str):
     # concatenate the list into a single tensor
     train_images = torch.cat(train_images)
     train_target = torch.cat(train_target)
-    # perform data 
-    
+
+    # perform data preprocessing
     test_images: torch.Tensor = torch.load(f"{raw_dir}/test_images.pt")
     test_target: torch.Tensor = torch.load(f"{raw_dir}/test_target.pt")
 
@@ -40,5 +45,37 @@ def make_data(raw_dir: str, processed_dir: str):
     torch.save(test_images, f"{processed_dir}/test_images.pt")
     torch.save(test_target, f"{processed_dir}/test_target.pt")
 
-if __name__ == '__main__':
-    make_data()
+@click.command()
+@click.option("--raw_dir", default="data/raw", help="Path to raw data directory")
+@click.option("--processed_dir", default="data/processed", help="Path to processed data directory")
+def make_data_v2(raw_dir: str, processed_dir: str):
+    """Perform data preprocessing on the .pt files."""
+    N_IMAGES = 4
+    start_idx = 6
+    # load data
+    train_images, train_target = [], []
+    for i in range(start_idx, start_idx + N_IMAGES):
+        # put the images in a list
+        train_images.append(torch.load(f"{raw_dir}/train_images_{i}.pt"))
+        train_target.append(torch.load(f"{raw_dir}/train_target_{i}.pt"))
+
+    # concatenate the list into a single tensor
+    train_images = torch.cat(train_images)
+    train_target = torch.cat(train_target)
+
+    # perform data preprocessing
+    train_images = train_images.unsqueeze(1).float()
+    train_target = train_target.long()
+
+    train_images = standardize(train_images)
+
+    print(train_images.size())
+
+    torch.save(train_images, f"{processed_dir}/train_images.pt")
+    torch.save(train_target, f"{processed_dir}/train_target.pt")
+
+cli.add_command(make_data)
+cli.add_command(make_data_v2)
+
+if __name__ == "__main__":
+    cli()
